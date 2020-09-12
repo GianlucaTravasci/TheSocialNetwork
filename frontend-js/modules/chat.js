8b229281d5fcd7cf5176d6eb7216f700fddc9413
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-12 10:44:33 +0200

    adjustement

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -36,7 +36,7 @@ export default class Chat {
         }
         this.openedYet = true;
         this.chatWrapper.classList.add('chat--visible');
-        this.chatField.focus()
+        this.chatField.focus();
     }
 
     closeChat() {
